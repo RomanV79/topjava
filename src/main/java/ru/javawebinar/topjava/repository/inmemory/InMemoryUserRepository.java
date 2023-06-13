546a0ca953f01668d40d0a0e1dca9dcfcd024235
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-06-13 13:59:22 +0300

    hw02L point 1,2,3

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -7,39 +7,60 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger();
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return repository.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+            repository.put(user.getId(), user);
+            return user;
+        }
+        return repository.computeIfPresent(user.getId(), (id, oldUser)-> user);
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return repository.values().stream().sorted(Comparator.comparing(user -> user.getName())).collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        User requareUser = null;
+        for (Map.Entry<Integer, User> user:repository.entrySet()) {
+            if (user.getValue().getEmail().equals(email)){
+                requareUser = (User) user;
+            }
+        }
+        return requareUser;
+
+        // не уверен что такой stream верен, поэтому обернул в коммент
+//        User requareUser = repository.values().stream().filter(user -> user.getEmail().equals(email)).collect(Collectors.toList()).get(0);
     }
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/inmemory/InMemoryMealRepository.java
@@ -5,42 +5,46 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private final Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal->save(1, meal));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Integer userId, Meal meal) {
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            repository.get(userId).put(meal.getId(), meal);
             return meal;
         }
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return repository.get(userId).computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(Integer userId, int id) {
+        return repository.get(userId).remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(Integer userId, int id) {
+        return repository.get(userId).get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(Integer userId) {
+        return repository.get(userId).values().stream()
+                .sorted(Comparator.comparing(meal->meal.getDate()))
+                .collect(Collectors.toList());
     }
 }
 
