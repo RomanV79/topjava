8bdc9e5adf8ddabeb601ffba45e407be44ecdb91
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-06-13 17:38:15 +0300

    hw02: point 4(not all)

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,37 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.security.PublicKey;
+import java.util.List;
 
 public class MealService {
 
     private MealRepository repository;
+    public MealService(MealRepository repository){
+        this.repository = repository;
+    }
+
+    public Meal create(Integer userId, Meal meal){
+        return repository.save(userId, meal);
+    }
+
+    public void delete(Integer userId, int id){
+        repository.delete(userId, id);
+    }
+
+    public Meal get(Integer userId, int id){
+        return repository.get(userId, id);
+    }
+
+    public List<Meal> getAll(Integer userId){
+        return (List<Meal>) repository.getAll(userId);
+    }
+
+    public void update(Integer userId, Meal meal){
+        repository.save(userId, meal);
+    }
+
 
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,34 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
+import java.util.List;
+
 public class MealRestController {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
     private MealService service;
+
+    public List<Meal> getAll(Integer userId){
+        log.info("getAll");
+        return service.getAll(userId);
+    }
+
+    public Meal get(Integer userId, int id){
+        log.info("get {}", id);
+        return service.get(userId, id);
+    }
+
+    public Meal create(Integer userId, Meal meal){
+        log.info("create {} with userid={}", meal, userId);
+        return service.create(userId, meal);
+    }
+
+    public void update(Integer userId, Meal meal){
+        log.info("update userId={} ; meal {}", userId, meal);
+    }
 
 }
