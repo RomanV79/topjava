5b6f57bfae1b3257dc547085b0e8d1d2278bbc4f
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-06-14 08:10:25 +0300

    hw02: fix#2 and point 5

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -10,6 +11,7 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
 public class MealService {
+
 
     private MealRepository repository;
 
