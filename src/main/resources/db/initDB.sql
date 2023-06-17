8268d31efcd923f6467c97546131661820c1c454
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-06-17 15:59:17 +0300

    feat: add TABLE meals to init script

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -23,3 +23,13 @@ CREATE TABLE user_role
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    date_time TIMESTAMP NOT NULL,
+    description VARCHAR NOT NULL,
+    calories INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_unique_date_time_idx ON meals (date_time);
